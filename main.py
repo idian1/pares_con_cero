3ab568b7463fff1be5ed2aa0f24234fc998e78df
idian1 <108105770+idian1@users.noreply.github.com>
2022-06-23 22:52:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ while c < n:
     c = c+1
 input(" ")
 # Cuando el usuario ingresa el valor para la variable "n", se generea un ciclo regulado por la variable "c" la cual
-# aumentara en 1 con cada repeticion hasta alcanzar el valor de "n-1", ademas en cada repeticion imprimira la
+# aumentara en 1 con cada repeticion hasta alcanzar el valor de "n-1", ademas en cada repeticion imprime la
 # variable "p" la cual es el resultado de el numero del ciclo actual multiplicado por 2 lo que dara como resultado
-# "n" numero de pares.
-# Este codigo utiliza el ciclo "while" por su simplisidad y facilidad de uso
+# "n" numero de pares incluido el cero.
